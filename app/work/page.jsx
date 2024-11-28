3bc02d817aa8ded7465fe80790ddac60280d39bf
wsnrfidev <bisabismillah04@gmail.com>
2024-11-29 04:37:48 +0700

    update

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -5,6 +5,8 @@ import React, { useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 

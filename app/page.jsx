0b2d385fb5e45d065ad0ad5295c41a900e7258cf
wsnrfidev <bisabismillah04@gmail.com>
2024-11-29 05:05:35 +0700

    update CV button

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl ">Software Developer</span>
+            <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Wisnu Rafi</span>
             </h1>
@@ -20,19 +20,25 @@ const Home = () => {
               proficient in various programming languages and technologies.
             </p>
             {/* btn + socials */}
-            <div className="flex flex-col xl:flex-row items0center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              {/* Download CV */}
+              <a
+                href="/assets/CV.pdf" 
+                download 
               >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:text-primary hover:transtion-all duration-500"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:text-primary hover:transition-all duration-500"
                 />
               </div>
             </div>
